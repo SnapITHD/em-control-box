9fdcbf70740706d9f17c6729f58f34ceac53291a
steve <steven@snapithd.com>
2018-07-13 11:55:54 +1200

    Added basic shell for framework

---
+++ src/rec/snapithd.cpp
@@ -0,0 +1,6 @@
+#include "snapithd.h"
+
+// Take the cmdline optiona and split it into a array so we know which cameras are shd cameras.
+void ParseSHD(bool* barry,std::string cmdoption) {
+
+}
---
+++ src/rec/snapithd.h
@@ -0,0 +1,8 @@
+#ifndef SNAPIT_HD_H
+#define SNAPIT_HD_H
+
+#include <string>
+
+void ParseSHD(bool* barry,std::string cmdoption);
+
+#endif
